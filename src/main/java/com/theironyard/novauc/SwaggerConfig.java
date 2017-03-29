16241c1dc75fb02a7edc3c49d382f2eef8378328
D'Angelo Joyce <dangelomjoyce@gmail.com>
2017-03-29 11:44:15 -0400

    Prep for merge

--- src/main/java/com/theironyard/novauc/SwaggerConfig.java
+++ src/main/java/com/theironyard/novauc/SwaggerConfig.java
@@ -23,6 +23,7 @@ public class SwaggerConfig {
                 .pathMapping("/")
                 .apiInfo(metaData());
     }
+    
     private ApiInfo metaData() {
         ApiInfo apiInfo = new ApiInfo(
                 "People Group test",
